8a219f84ce0846befe7d0353c09a9f9f1c598102
chungngoc258 <tongngocchungbtqb@gmail.com>
2026-02-10 01:55:42 +0100

    Time: 0 ms (100.00%) | Memory: 19.3 MB (41.81%) - LeetSync

--- 118-pascals-triangle/pascals-triangle.py
+++ 118-pascals-triangle/pascals-triangle.py
@@ -1,33 +1,35 @@
 class Solution:
     def generate(self, numRows: int) -> List[List[int]]:
-        triangle = [[1]]
+        # ### Bottom-Up DP
+        # triangle = [[1]]
 
-        for i in range(1, numRows):
-            prev_row = triangle[-1]
-            row = [1]
+        # for i in range(1, numRows):
+        #     prev_row = triangle[-1]
+        #     row = [1]
 
-            for j in range(1, i):
-                row.append(prev_row[j-1] + prev_row[j])
+        #     for j in range(1, i):
+        #         row.append(prev_row[j-1] + prev_row[j])
 
-            row.append(1)
-            triangle.append(row)
+        #     row.append(1)
+        #     triangle.append(row)
 
-        return triangle
+        # return triangle
         
-        # if numRows == 1:
-        #     return [[1]]
+        ### Top-down DP (recursive)
+        if numRows == 1:
+            return [[1]]
 
-        # # Get previous pascal_triangle
-        # pascal_triangle = self.generate(numRows-1)
+        # Get previous pascal_triangle
+        pascal_triangle = self.generate(numRows-1)
         
-        # # Get current row
-        # nth_row=[1]
-        # for i in range(1, len(pascal_triangle[-1])):
-        #     nth_row.append(pascal_triangle[-1][i-1]+pascal_triangle[-1][i])
-        # nth_row.append(1)
+        # Get current row
+        nth_row=[1]
+        for i in range(1, len(pascal_triangle[-1])):
+            nth_row.append(pascal_triangle[-1][i-1]+pascal_triangle[-1][i])
+        nth_row.append(1)
         
-        # # Generate current triangle
-        # pascal_triangle.append(nth_row)
-        # return pascal_triangle
+        # Generate current triangle
+        pascal_triangle.append(nth_row)
+        return pascal_triangle
         
         
