38198202843574c58231b3a30539cd82a0b2b7cf
chungngoc258 <tongngocchungbtqb@gmail.com>
2026-02-15 20:56:38 +0100

    Time: 1 ms (30.90%) | Memory: 19.3 MB (65.15%) - LeetSync

--- 62-unique-paths/unique-paths.py
+++ 62-unique-paths/unique-paths.py
@@ -1,10 +1,18 @@
 import math
 
 class Solution:
-    def uniquePaths(self, m: int, n: int) -> int:
-        # There are m-1 steps down and n-1 steps right
-        len_path = m+n-2
+    def uniquePaths(self, m: int, n: int) -> int:      
+        # DP solution
+        dp = [1] * n
 
-        return int(math.factorial(len_path) / math.factorial(m-1) / math.factorial(n-1))
-
+        for _ in range(1, m):
+            for j in range(1, n):
+                dp[j] += dp[j-1]
+        return dp[-1]
         
+        # Mathematics solution
+        # # There are m-1 steps down and n-1 steps right
+        # return int(math.factorial(m+n-2) / math.factorial(m-1) / math.factorial(n-1))
+
+
+    
