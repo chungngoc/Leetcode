e86b9c6a7636eb45630f07d6c94baea7c8f34a5f
chungngoc258 <tongngocchungbtqb@gmail.com>
2026-03-31 18:49:50 -0400

    Time: 0 ms (100.00%) | Memory: 19.2 MB (69.59%) - LeetSync

--- 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
+++ 2999-check-if-strings-can-be-made-equal-with-operations-i/check-if-strings-can-be-made-equal-with-operations-i.py
@@ -1,10 +1,13 @@
 class Solution:
     def canBeEqual(self, s1: str, s2: str) -> bool:
-        # There are 2 options to choose i and j
-        # i = 0 and j = 2 OR i = 1 and j = 3
-        for (i,j) in [(0,2), (1,3)]:
-            set1 = set([s1[i], s1[j]])
-            set2 = set([s2[i], s2[j]])
-            if len(set1.difference(set2)) > 0:
-                return False
-        return True
+        # for (i,j) in [(0,2), (1,3)]:
+        #     set1 = set([s1[i], s1[j]])
+        #     set2 = set([s2[i], s2[j]])
+        #     if len(set1.difference(set2)) > 0:
+        #         return False
+        # return True
+
+        return (
+            sorted(s1[::2]) == sorted(s2[::2]) and
+            sorted(s1[1::2]) == sorted(s2[1::2])
+        )
